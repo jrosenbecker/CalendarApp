e139386fc5d510126045ff292398b75888965743
jrosenbecker <rosenbeckerjw@gmail.com>
2016-02-29 22:43:33 -0600

    Added layouts for a main calendar screen an add calendar screen. Worked on implementing adding a calendar so that it will appear in the list

--- app/src/main/java/com/example/joe/calendarapp/MainActivity.java
+++ app/src/main/java/com/example/joe/calendarapp/MainActivity.java
@@ -1,13 +1,67 @@
 package com.example.joe.calendarapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static ListView calendarListView;
+    private static List<String> calendarList;
+    private static ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        calendarListView = (ListView) findViewById(R.id.calendarList);
+
+
+        // TODO: load the list from a saved resource
+        calendarList = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, calendarList);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.add_calendar_button:
+                Intent intent = new Intent(getApplicationContext(), AddCalendarActivity.class);
+                startActivityForResult(intent, 1);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK) {
+            addCalendarToList(data.getStringExtra("Add"));
+        }
+    }
+
+    public void addCalendarToList(String name)
+    {
+        calendarList.add(name);
+        adapter.notifyDataSetChanged();
     }
 }
